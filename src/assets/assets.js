7f737450ffc1831d9af37ef4e2a560fba995af4b
odusdegreat <tobiodubote@gmail.com>
2025-03-18 18:57:27 +0100

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,8 @@
+import three_icon from "./3.jpg";
+import smile_icon from "./smile.jpeg";
+import today_icon from "./today's top hit.jpeg";
+import global_icon from "./top 50 global.jpeg";
+import nigeria_icon from "./top 50 nigeria.jpeg";
+import uk_icon from "./top 50 uk.jpeg";
+
+export const assets = {};
