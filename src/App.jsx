45446a4c4df1bbcb8f33b7c531f475fbe901e316
odusdegreat <tobiodubote@gmail.com>
2025-03-21 16:42:54 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Sidebar from "./components/sidebar";
 const App = () => {
   return (
     <div className="h-screen bg-black">
-      <div className="h-[90%] flex"></div>
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
     </div>
   );
 };
