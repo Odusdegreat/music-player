cdf3c0fce1f04f3883eca48e97062716eaa3256a
odusdegreat <tobiodubote@gmail.com>
2025-03-22 17:35:07 +0100

    initial commit

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const display = () => {
-  return <div>display</div>;
+  return (
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"></div>
+  );
 };
 
 export default display;
