f87abda8c2d4f8c210fe819b7fcc25ba319a9b99
odusdegreat <tobiodubote@gmail.com>
2025-03-22 17:51:28 +0100

    initial commit

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -3,7 +3,11 @@ import { Router } from "react-router-dom";
 
 const display = () => {
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"></div>
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+      <Routes>
+        <Route path="/" element={<DisplayHome />} />
+      </Routes>
+    </div>
   );
 };
 
