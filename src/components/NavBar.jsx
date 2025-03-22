73e8da0712ef4fccb0e6d972bbcceedfe465aeb8
odusdegreat <tobiodubote@gmail.com>
2025-03-23 00:30:43 +0100

    initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,6 +20,9 @@ const NavBar = () => {
           <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
             Explore Premium
           </p>
+          <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">
+            Install App
+          </p>
         </div>
       </div>
     </>
