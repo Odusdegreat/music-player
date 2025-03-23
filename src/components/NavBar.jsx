18e861dd97a3f5f76642628ff0125be5ac4d26a8
odusdegreat <tobiodubote@gmail.com>
2025-03-23 16:33:35 +0100

    initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,7 +30,10 @@ const NavBar = () => {
       </div>
       <div className="flex items-center gap-2 mt-4">
         <p className="bg-white text-black px-4 py-1 rounded-2xl">All</p>
-        <p className="bg-black px-4 py-1 rounded-2xl">Music</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
+          Podcasts
+        </p>
       </div>
     </>
   );
