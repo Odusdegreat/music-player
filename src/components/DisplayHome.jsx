0c4d9f08747f80151d936c1782ae9c6428d8797a
odusdegreat <tobiodubote@gmail.com>
2025-03-25 18:02:48 +0100

    initial commit

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -21,6 +21,20 @@ const DisplayHome = () => {
           ))}
         </div>
       </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
