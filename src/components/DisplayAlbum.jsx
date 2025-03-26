832d8a93480c71be470a57a84f3d9115ecf4a2b4
odusdegreat <tobiodubote@gmail.com>
2025-03-26 14:14:55 +0100

    initial commit

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { useParams } from "react-router-dom";
 
 const DisplayAlbum = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <>
       <NavBar />
