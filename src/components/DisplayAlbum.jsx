fb0757a42b59bbcddf3a664b037ad4c7933949ef
odusdegreat <tobiodubote@gmail.com>
2025-03-26 17:29:57 +0100

    initial commit

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -42,6 +42,7 @@ const DisplayAlbum = () => {
         >
           <p className="text-white">
             <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
+            <img className="inline w-10 mr-5" src={item.image} alt="" />
           </p>
         </div>
       ))}
