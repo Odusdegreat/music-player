1e2e71cf677df1efb4d5f4e6347466f4c9fd90b3
odusdegreat <tobiodubote@gmail.com>
2025-03-26 14:21:57 +0100

    initial commit

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import NavBar from "./NavBar";
 import { useParams } from "react-router-dom";
+import { albumsData } from "../assets/assets";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
-  console.log(id);
+  const albumsData = albumsData[id];
+  console.log(albumsData);
   return (
     <>
       <NavBar />
+      <div className="mt-10 flex gap-8 flex-col"></div>
     </>
   );
 };
