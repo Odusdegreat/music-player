053bc73579cad0235891235d9b86f11c9d091850
odusdegreat <tobiodubote@gmail.com>
2025-03-26 17:59:14 +0100

    initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import {} from "../assets/assets";
 import { SlArrowLeft } from "react-icons/sl";
 import { SlArrowRight } from "react-icons/sl";
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
           <SlArrowLeft
             size={25}
+            onClick={() => navigate}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
           />
           <SlArrowRight
             size={25}
+            onClick={() => navigate}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
           />
         </div>
