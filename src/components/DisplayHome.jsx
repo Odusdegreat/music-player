ec487e82543412b47d00faf0555f7d5226dcf5db
odusdegreat <tobiodubote@gmail.com>
2025-03-26 13:58:11 +0100

    initial commit

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 import { albumsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
 import { songsData } from "../assets/assets";
-import SongItem from "../assets/SongItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
