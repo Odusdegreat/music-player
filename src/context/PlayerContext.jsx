df51f4138dcd68ea1ebd8925afe5de23b922b392
odusdegreat <tobiodubote@gmail.com>
2025-03-28 23:59:51 +0100

    initial commit

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,8 +1,8 @@
 import { createContext } from "react";
 
-const Player = createContext();
+export const PlayerContext = createContext();
 
-const PlayerContextProvider = () => {
+const PlayerContextProvider = (props) => {
   const contextValue = {};
   return (
     <PlayerContext.Provider value={contextValue}>
