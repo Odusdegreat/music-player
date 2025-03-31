a057919dae92f0a12594e0c98ad48234edfa4544
odusdegreat <tobiodubote@gmail.com>
2025-03-31 22:25:27 +0100

    initial commit

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useRef } from "react";
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
 
@@ -8,12 +9,40 @@ const PlayerContextProvider = (props) => {
   const seekBar = useRef;
   [];
 
-  const [track, setTrack] = useState;
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setPlayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
+    },
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
+
+  const play = () => {
+    audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  condt pause = () => {
+    audioRef.current.pause()
+    setPlayStatus(false);
+  }
 
   const contextValue = {
     audioRef,
     seekBar,
     seekBg,
+    track,
+    setTrack,
+    playStatus,
+    setPlayStatus,
+    time,
+    setTime,
+    play,pause,
   };
   return (
     <PlayerContext.Provider value={contextValue}>
