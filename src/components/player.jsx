3873961c7ba95fd336382f5fb9f18ba9fc5a5071
odusdegreat <tobiodubote@gmail.com>
2025-03-31 22:29:03 +0100

    initial commit

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -15,7 +15,8 @@ import { MdOutlineZoomOutMap } from "react-icons/md";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-  const { seekBar, seekBg } = useContext(PlayerContext);
+  const { seekBar, seekBg, playStatus, play, pause } =
+    useContext(PlayerContext);
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
