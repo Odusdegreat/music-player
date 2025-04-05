4b58c05c10f0896d7607de3e11f9cde9355786cf
odusdegreat <tobiodubote@gmail.com>
2025-04-05 20:41:06 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
-import Player from "./components/Player";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player.jsx";
 import Display from "./components/Display";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Search from "./components/Search";
 
 import { PlayerContext } from "./context/PlayerContext";
+
 const App = () => {
   const { audioRef, track } = useContext(PlayerContext);
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
+        <Sidebar />
         <Display />
       </div>
       <Player />
@@ -18,11 +21,6 @@ const App = () => {
         <Route path="/search" element={<Search />} />
         {/* other routes */}
       </Routes>
-      <Routes>
-        <Player />
-      </Routes>
-
-      <Search />
     </div>
   );
 };
